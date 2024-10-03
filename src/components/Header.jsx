2f9e92c2513344eef0a39e59d443d2ae97deca7e
Juan Ignacio Esquercia <150676551+JuanIEsquercia@users.noreply.github.com>
2024-10-03 19:26:22 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,7 +21,7 @@ function Header() {
     <header className="bg-white shadow-sm dark:bg-gray-800">
       <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
         <Link to="/" className="text-3xl font-bold text-gray-900 dark:text-white">
-          Corrientes Stays
+          Alojamientos Corrientes
         </Link>
         <button
           onClick={toggleDarkMode}
